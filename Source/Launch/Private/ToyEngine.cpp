e0664fc240ffe8b51d74cf1ceda1303a08cef110
Vikcoo <1363370470@qq.com>
2025-08-09 01:02:35 +0800

    新增三方库log,glfw和程序入口

---
+++ Source/Launch/Private/ToyEngine.cpp
@@ -0,0 +1,15 @@
+#include "ToyEngine.h"
+
+using namespace std;
+
+int ToyEngine::Run()
+{
+    cout << "Hello World from ToyEngine!" << endl;
+    cout << "ToyEngine is starting..." << endl;
+    return 0;
+}
+
+int main()
+{
+    return ToyEngine::Run();
+}
---
+++ Source/Launch/Public/ToyEngine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <iostream>
+
+class ToyEngine
+{
+public:
+    static int Run();
+};
